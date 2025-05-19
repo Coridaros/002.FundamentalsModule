dbc7bfe79c3d40ae7257e2477e47b31e88f80138
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-19 22:53:02 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/002.Passed/Passed.cs
@@ -0,0 +1,9 @@
+﻿//Basic syntax, conditional statements and loops, Task 002
+//19.05.2025, 22:44
+
+double grade = double.Parse(Console.ReadLine());
+
+if(grade >= 3.00)
+{
+    Console.WriteLine("Passed!");
+}
