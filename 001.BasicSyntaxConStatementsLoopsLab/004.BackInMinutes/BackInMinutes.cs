1ab787253cd7ac8d1b01596963e4881faa6f9bd9
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-19 23:00:23 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/004.BackInMinutes/BackInMinutes.cs
@@ -0,0 +1,27 @@
+﻿//Basic syntax, conditional statements and loops, Task 004
+//19.05.2025, 22:56
+
+int hours = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+
+minutes += 30;
+
+if(minutes > 59)
+{
+    hours++;
+    minutes -= 60;
+}
+
+if(hours > 23)
+{
+    hours -= 24;
+}
+
+if(minutes < 10)
+{
+    Console.WriteLine($"{hours}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hours}:{minutes}");
+}
