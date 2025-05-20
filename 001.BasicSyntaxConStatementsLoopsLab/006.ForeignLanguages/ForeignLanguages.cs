2afc1934404798f7b3d81856bae48de90df6b2fb
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-21 01:00:17 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/006.ForeignLanguages/ForeignLanguages.cs
@@ -0,0 +1,20 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 006
+//21.05.2025, 00:01
+
+string country = Console.ReadLine().ToLower();
+
+switch(country)
+{
+    case "england":
+    case "usa":
+        Console.WriteLine("English");
+        break;
+    case "spain":
+    case "argentina":
+    case "mexico":
+        Console.WriteLine("Spanish");
+        break;
+    default:
+        Console.WriteLine("unknown");
+        break;
+}
