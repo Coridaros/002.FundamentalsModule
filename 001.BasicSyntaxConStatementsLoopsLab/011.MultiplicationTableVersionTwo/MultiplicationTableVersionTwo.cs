0d0969ec524fdf65f6b2d2fa5c4f8d2cd0b10513
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-22 18:43:15 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/011.MultiplicationTableVersionTwo/MultiplicationTableVersionTwo.cs
@@ -0,0 +1,17 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 011
+//21.05.2025, 01:19
+
+int n = int.Parse(Console.ReadLine());
+int times = int.Parse(Console.ReadLine());
+
+if(times <= 10)
+{
+    for(int i = times; i <= 10; i++)
+    {
+        Console.WriteLine($"{n} X {i} = {n * i}");
+    }
+}
+else
+{
+    Console.WriteLine($"{n} X {times} = {n * times}");
+}
