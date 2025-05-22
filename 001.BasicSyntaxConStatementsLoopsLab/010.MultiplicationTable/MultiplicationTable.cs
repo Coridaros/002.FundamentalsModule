fb56c386deb7398e04ec7cfb2ed868343152ce5c
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-22 18:42:59 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/010.MultiplicationTable/MultiplicationTable.cs
@@ -0,0 +1,9 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 010
+//21.05.2025, 01:16
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{n} X {i} = {n * i}");
+}
