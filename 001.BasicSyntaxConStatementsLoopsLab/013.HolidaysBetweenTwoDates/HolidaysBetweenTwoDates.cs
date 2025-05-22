061d78b2829ec5c4c1fd04709bf269b8d9b44e69
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-22 18:44:02 +0300

    need fix

---
+++ 001.BasicSyntaxConStatementsLoopsLab/013.HolidaysBetweenTwoDates/HolidaysBetweenTwoDates.cs
@@ -0,0 +1,14 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 013
+//21.05.2025, 01:29
+
+using System.Globalization;
+
+var startDate = DateTime.ParseExact(Console.ReadLine(),
+    "dd.m.yyyy", CultureInfo.InvariantCulture);
+var endDate = DateTime.ParseExact(Console.ReadLine(),
+    "dd.m.yyyy", CultureInfo.InvariantCulture);
+var holidaysCount = 0;
+for (var date = startDate; date <= endDate; date.AddDays(1))
+    if (date.DayOfWeek == DayOfWeek.Saturday &&
+        date.DayOfWeek == DayOfWeek.Sunday) holidaysCount++;
+Console.WriteLine(holidaysCount);
