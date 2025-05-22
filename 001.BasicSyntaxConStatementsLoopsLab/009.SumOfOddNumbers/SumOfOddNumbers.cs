e80b94498fe6e6d733ef537516ce107f8ac5b218
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-22 18:42:40 +0300

    add

---
+++ 001.BasicSyntaxConStatementsLoopsLab/009.SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Basic syntax, conditional statements and loops - lab, Task 009
+//21.05.2025, 01:06
+
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+int odd = 1;
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine(odd);
+    sum += odd;
+    odd += 2;
+}
+
+Console.WriteLine($"Sum: {sum}");
