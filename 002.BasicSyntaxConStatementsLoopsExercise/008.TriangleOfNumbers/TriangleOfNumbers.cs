673a383906b139cfed7fa9c81e8d0d5324256e9e
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-09 01:06:38 +0300

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/008.TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,20 @@
+﻿//Basic syantax, conditional statements and loops - exercise, Task 008
+//09.06.2025, 00:47
+
+public class TriangleOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
