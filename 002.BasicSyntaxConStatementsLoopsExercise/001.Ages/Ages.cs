7e226517349be615b8c3c9dedb7dda876d873572
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-09 01:04:18 +0300

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/001.Ages/Ages.cs
@@ -0,0 +1,31 @@
+﻿//Basic syntax, conditional statements and loop - exercise, Task 001
+//08.06.2025, 23:21
+
+public class Ages
+{
+    static void Main()
+    {
+        int age = int.Parse(Console.ReadLine());
+
+        if(age >= 0 && age <= 2)
+        {
+            Console.WriteLine("baby");
+        }
+        else if(age >= 3 && age <= 13)
+        {
+            Console.WriteLine("child");
+        }
+        else if(age >= 14 && age <= 19)
+        {
+            Console.WriteLine("teenager");
+        }
+        else if(age >= 20 && age <= 65)
+        {
+            Console.WriteLine("adult");
+        }
+        else if(age >= 66)
+        {
+            Console.WriteLine("elder");
+        }
+    }
+}
