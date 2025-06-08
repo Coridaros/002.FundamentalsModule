171c1ebf44bafc0ef86a107e4263b13f24e5c87d
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-09 01:05:33 +0300

    add

---
+++ 002.BasicSyntaxConStatementsLoopsExercise/005.Login/Login.cs
@@ -0,0 +1,40 @@
+﻿//Basic syntax, conditional statements and loop - exercise, Task 005
+//09.06.2025, 00:09
+
+public class Login
+{
+    static void Main()
+    {
+        string username = Console.ReadLine();
+
+        int wrongPassCnt = 0;
+        string password = string.Empty;
+
+        for(int i = username.Length - 1; i >= 0; i--)
+        {
+            password += username[i];
+        }
+
+        while(true)
+        {
+            string pass = Console.ReadLine();
+
+            if(wrongPassCnt >= 3)
+            {
+                Console.WriteLine($"User {username} blocked!");
+                break;
+            }
+
+            if(pass != password)
+            {
+                wrongPassCnt++;
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+            else
+            {
+                Console.WriteLine($"User {username} logged in.");
+                break;
+            }
+        }
+    }
+}
